f8ac2aa31fb15af245ab3601c5d66c38f27196a1
James Robertson <robertsonj@atmroberj01.atdom.ad.agilex.com>
2013-06-13 13:28:48 -0400

    Initial Commit

---
+++ OpenCVTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenCVTest
+//
+//  Created by James Robertson on 6/13/13.
+//  Copyright (c) 2013 James Robertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenCVTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenCVTest
+//
+//  Created by James Robertson on 6/13/13.
+//  Copyright (c) 2013 James Robertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
