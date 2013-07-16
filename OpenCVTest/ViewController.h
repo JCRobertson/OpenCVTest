da5afbdae210a1a423221ad35fb11c757df47595
james@jamesrobertson.org <robertsonj@atldaniec01.atdom.ad.agilex.com>
2013-07-16 10:45:42 -0400

    XCode commit

--- OpenCVTest/ViewController.h
+++ OpenCVTest/ViewController.h
@@ -7,7 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <opencv2/highgui/cap_ios.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <math.h>
+#include <string.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CvVideoCameraDelegate>
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIButton *button;
+@property (strong, nonatomic) CvVideoCamera* videoCamera;
+@property Boolean isOn;
+- (IBAction)actionStart:(id)sender;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *typeOfDraw;
+- (IBAction)changePerspective:(id)sender;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *perpective;
 
 @end
